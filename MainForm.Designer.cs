936c97fe6edf162dbfeaf7ae2fc0f84763a06f5c
Vidmantas Bakstys <vidas007@yahoo.com>
2018-09-23 16:37:59 +0300

    emgu cv added

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -30,13 +30,15 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(143, 150);
+            this.button1.Location = new System.Drawing.Point(191, 185);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(171, 57);
+            this.button1.Size = new System.Drawing.Size(228, 70);
             this.button1.TabIndex = 0;
             this.button1.Text = "Select Video";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,21 +46,34 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(460, 150);
+            this.button2.Location = new System.Drawing.Point(613, 185);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(171, 57);
+            this.button2.Size = new System.Drawing.Size(228, 70);
             this.button2.TabIndex = 1;
             this.button2.Text = "Webcam";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(421, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(197, 62);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "EmguCam";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.ResumeLayout(false);
@@ -69,5 +84,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
