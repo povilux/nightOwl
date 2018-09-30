f60c4361fda527e0035b8282400105c0408bff75
Vidmantas Bakstys <vidas007@yahoo.com>
2018-09-30 07:14:31 +0300

    face recognizer & more

--- Program.cs
+++ Program.cs
@@ -17,6 +17,7 @@ namespace nightOwl
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+
         }
     }
 }
