b6dcf54b78463fdcd9d738bf343329f2540e0984
Aurimas <aurimasko@gmail.com>
2018-10-22 17:15:47 +0300

    struct

--- Person.cs
+++ Person.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace nightOwl
 {
-    public class Person
+    public struct Person
     {
         public string Name { get; set; }
         public string BirthDate { get; set; }
