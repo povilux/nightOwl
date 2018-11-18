e69c8b0c7bc038c187e8a9602385168213269a5d
povilux <povilas.zar@gmail.com>
2018-11-19 01:35:02 +0200

    Created views, added navigation between them

--- NightOwl/NightOwl.Xamarin/NightOwl.Xamarin/Views/AddPerson.xaml.cs
+++ NightOwl/NightOwl.Xamarin/NightOwl.Xamarin/Views/AddPerson.xaml.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
-namespace NightOwl.Xamarin
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NightOwl.Xamarin.Views
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddPerson : ContentPage
     {
-        public MainPage()
+        public AddPerson()
         {
             InitializeComponent();
         }
