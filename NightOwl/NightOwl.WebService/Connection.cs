4bbc6c0c71b6fa031adc8c594b4502e467bba4d6
Aurimas <aurimasko@gmail.com>
2018-11-26 18:09:10 +0200

    test

--- NightOwl/NightOwl.WebService/Connection.cs
+++ NightOwl/NightOwl.WebService/Connection.cs
@@ -9,7 +9,5 @@ namespace NightOwl.WebService
     {
         public static string sqlConnection = @"Server = tcp:nightowl.database.windows.net,1433; Initial Catalog = nightOwl; Persist Security Info = False; User ID =aurimasko; Password = Topas123; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;";
 
-
-
     }
 }
