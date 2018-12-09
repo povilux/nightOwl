c32a0319fb9a7901d8edc2f02395b93dfd1404a0
Aurimas <aurimasko@gmail.com>
2018-12-09 15:19:23 +0200

    merged

---
+++ NightOwl/NightOwl.Xamarin/NightOwl.Xamarin.UWP/VideoRecognitionService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.Playback;
+using Xamarin.Forms;
+
+namespace NightOwl.Xamarin.UWP
+{
+    public class VideoRecognitionService
+    {
+        public VideoRecognitionService() { }
+
+        public View CreateMediaPlayerElement()
+        {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            return (View)mediaPlayer;
+        }
+    }
+}
