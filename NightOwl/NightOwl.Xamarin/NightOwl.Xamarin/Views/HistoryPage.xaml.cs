66f5e620647057e2cdb7ab4cc61f4ceb9d3d440b
Aurimas <aurimasko@gmail.com>
2018-12-09 22:10:33 +0200

    api implementation

--- NightOwl/NightOwl.Xamarin/NightOwl.Xamarin/Views/HistoryPage.xaml.cs
+++ NightOwl/NightOwl.Xamarin/NightOwl.Xamarin/Views/HistoryPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using NightOwl.Xamarin.Components;
+using NightOwl.Xamarin.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,9 +18,11 @@ namespace NightOwl.Xamarin.Views
         private int currentPageNumber = 1;
         private int numberOfPages = 0;
         private int numberOfItems = 0;
-        private List<PersonHistory> historyListToShow;    
+        private List<PersonHistory> historyListToShow;
 
-		public HistoryPage()
+        private IPersonHistoryService _PersonHistoryService;
+        
+        public HistoryPage()
 		{
 			InitializeComponent();
             filterPicker.SelectedIndex = 0;
@@ -27,14 +30,26 @@ namespace NightOwl.Xamarin.Views
             pageNumberLabel.IsVisible = false;
             previousPageButton.IsVisible = false;
             nextPageButton.IsVisible = false;
+
+            personIDButton.IsVisible = false;
+            personNameButton.IsVisible = false;
+            personDateButton.IsVisible = false;
+
             filterPicker.SelectedIndexChanged += FilterPickerIndexChanged;
             historyListToShow = new List<PersonHistory>();
-            imageToShow = null;
+
+            _PersonHistoryService = new PersonHistoryService();
+
             listToShow.ItemSelected += (object sender, SelectedItemChangedEventArgs e) =>
             {
                 PersonHistory selectedPerson = (PersonHistory)e.SelectedItem;
 
-                // imageToShow = ?
+                if (!string.IsNullOrEmpty(selectedPerson.SourceFaceUrl))
+                {
+                    imageToShow.IsVisible = true;
+
+                    imageToShow.Source = ImageSource.FromUri(new Uri(selectedPerson.SourceFaceUrl));
+                }
             };
 		}
 
@@ -52,34 +67,57 @@ namespace NightOwl.Xamarin.Views
             }
         }
 
+        async Task GetHistoryPages()
+        {
+            personIDButton.IsVisible = true;
+            personNameButton.IsVisible = true;
+            personDateButton.IsVisible = true;
+
+            if (filterPicker.SelectedIndex == 0)
+            {
+                var actionGetHistory = await _PersonHistoryService.GetPersonsHistoryList(currentPageNumber - 1);
+
+                if (actionGetHistory.Success)
+                {
+                    historyListToShow = actionGetHistory.Message.PersonHistories.ToList();
+                    numberOfItems = actionGetHistory.Message.PersonHistoriesCount;
+                }
+                else
+                {
+                    await DisplayAlert("asd", actionGetHistory.Error, "asd");
+                }
+            }
+            else if (filterPicker.SelectedIndex == 1)
+            {
+                if (string.IsNullOrEmpty(entry.Text))
+                {
+                    await DisplayAlert("Error", "No search value", "Close");
+                    return;
+                }
+
+                name = entry.Text;
+                var actionGetHistory = await _PersonHistoryService.GetPersonHistoryByName(name, currentPageNumber - 1);
+
+                if (actionGetHistory.Success)
+                {
+                    historyListToShow = actionGetHistory.Message.PersonHistories.ToList();
+                    numberOfItems = actionGetHistory.Message.PersonHistoriesCount;
+                }
+                else
+                {
+                    await DisplayAlert("Error", actionGetHistory.Error, "Error");
+                }
+            }
+        }
+
         async void OnSearchButtonClicked(object sender, EventArgs e)
         {
             currentPageNumber = 1;
             numberOfItems = 0;
-            imageToShow = null;
+            imageToShow.Source = null;
 
-            if (filterPicker.SelectedIndex == 0)
-            {
-                // get with no filter:                  currentPage = 1, t.y. norime gauti pirmus 10 irasu is db
-                    
-                //  historyListToShow = ?         //  gaunam 10 pirmu irasu is db kaip List<PersonHistory>            
-                //  numberOfItems = ?             //  gaunam visu tinkamu irasu skaiciu db
-            }
-            else
-            {
-                if(filterPicker.SelectedIndex == 1)
-                {
-                    name = entry.Text;               // turime varda kintamajam "name", kuri vartotojas nurode kaip filtra
-
-                    // get filtered by name:            currentPage = 1, t.y. norime gauti pirmus 10 irasu is db
-                     
-                    // historyListToShow = ?       //  gaunam 10 pirmu irasu is db kaip List<PersonHistory>
-                    // numberOfItems = ?           //  gaunam visu tinkamu irasu skaiciu db
-                }
-            }
-
-            // numberOfItems = 35;          // test
-
+            await GetHistoryPages();
+ 
             if (numberOfItems > numberOfItems / 10 * 10)
             {
                 numberOfPages = numberOfItems / 10 + 1;
@@ -93,89 +131,48 @@ namespace NightOwl.Xamarin.Views
             {
                 nextPageButton.IsVisible = true;
             }
+            pageNumberLabel.Text = currentPageNumber.ToString();
             pageNumberLabel.IsVisible = true;
-
-            /*          test
-            PersonHistory testph1 = new PersonHistory();
-            PersonHistory testph2 = new PersonHistory();
-            PersonHistory testph3 = new PersonHistory();
-            testph1.PersonID = 1;
-            testph2.PersonID = 2;
-            testph3.PersonID = 3;
-            testph1.PersonName = "Jonas";
-            testph2.PersonName = "Algis";
-            testph3.PersonName = "Tomas";
-            testph1.PersonDate = "1940";
-            testph2.PersonDate = "2010";
-            testph3.PersonDate = "1850";
-            historyListToShow.Add(testph1);
-            historyListToShow.Add(testph2);
-            historyListToShow.Add(testph3);
-            */
 
             BindingContext = historyListToShow;
         }
 
-        void OnNextButtonClicked(object sender, EventArgs e)
+        async void OnNextButtonClicked(object sender, EventArgs e)
         {
             currentPageNumber++;
-            if(currentPageNumber == numberOfPages)
+            pageNumberLabel.Text = currentPageNumber.ToString();
+            if (currentPageNumber == numberOfPages)
             {
                 nextPageButton.IsVisible = false;
             }
             previousPageButton.IsVisible = true;
 
-            GetHistoryPages();
+            await GetHistoryPages();
+            BindingContext = historyListToShow;
+
         }
 
-        void OnPreviousButtonClicked(object sender, EventArgs e)
+        async void OnPreviousButtonClicked(object sender, EventArgs e)
         {
             currentPageNumber--;
-            if(currentPageNumber == 1)
+            pageNumberLabel.Text = currentPageNumber.ToString();
+
+            if (currentPageNumber == 1)
             {
                 previousPageButton.IsVisible = false;
             }
             nextPageButton.IsVisible = true;
 
-            GetHistoryPages();
+            await GetHistoryPages();
+            BindingContext = historyListToShow;
+
         }
 
-        async void GetHistoryPages()
-        {
-            if (filterPicker.SelectedIndex == 0)
-            {
-                // get with no filter:
 
-                // current page turi reiksme, kuri atspindi, kuriuos elementus norime gauti:
-                // jei currentPage = 1, tai 0-9, jei currentPage = 2, tai 10-19 ir t.t.
-                //  numberOfItems keisti nebereikia, nes jis ir taip inicializuotas
-
-                //  historyListToShow = ?         //  gaunam 1-10 irasu is db kaip List<PersonHistory>         
-
-            }
-            else
-            {
-                if (filterPicker.SelectedIndex == 1)
-                {
-                    name = entry.Text;               // turime varda, kuri vartotojas nurode kaip filtra
-
-                    // get filtered by name:           
-
-                    // currentPage turi reiksme, kuri atspindi, kuriuos elementus norime gauti:
-                    // jei currentPage = 1, tai 0-9, jei currentPage = 2, tai 10-19 ir t.t.
-                    //  numberOfItems keisti nebereikia, nes jis ir taip inicializuotas
-
-                    // historyListToShow = ?       //  gaunam 1-10 irasu is db kaip List<PersonHistory>
-
-                }
-            }
-
-            BindingContext = historyListToShow;
-        } 
 
         void OnIDButtonClicked(object sender, EventArgs e)
         {
-            var peopleInOrder = (historyListToShow.OrderBy(PersonHistory => PersonHistory.PersonID)).ToList();
+            var peopleInOrder = (historyListToShow.OrderBy(PersonHistory => PersonHistory.PersonId)).ToList();
             historyListToShow = peopleInOrder;
             BindingContext = historyListToShow;
         }
@@ -189,7 +186,7 @@ namespace NightOwl.Xamarin.Views
 
         void OnPersonDateButtonClicked(object sender, EventArgs e)
         {
-            var peopleInOrder = (historyListToShow.OrderBy(PersonHistory => PersonHistory.PersonDate)).ToList();
+            var peopleInOrder = (historyListToShow.OrderBy(PersonHistory => PersonHistory.Date)).ToList();
             historyListToShow = peopleInOrder;
             BindingContext = historyListToShow;
         }
